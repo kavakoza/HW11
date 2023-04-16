97cb904abf19ab1737fa1f4cd31cde3acde821d9
Tatyana Kazakova <119956548+kavakoza@users.noreply.github.com>
2023-04-16 20:24:07 +0400

    import summ

--- funcs.py
+++ funcs.py
@@ -1,4 +1,11 @@
-a = input()
-b = input()
-summ = a + b
-subtr = a - b
+def summ():
+    a = input()
+    b = input()
+    summ = a + b
+    print(summ)
+
+def subt():
+    a = input()
+    b = input()
+    subt = a - b
+    print(subt)
---
+++ main.py
@@ -0,0 +1,3 @@
+from funcs import *
+
+print(summ())
